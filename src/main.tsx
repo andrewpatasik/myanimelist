1c85fd306ed1ae945be8b02dca73a2a85ff17568
Andrew <andrew.yehezkiel17@gmail.com>
2023-07-31 23:03:01 +0800

    emoticon added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Global, css } from "@emotion/react";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootStyle = css({
+  ":root": {
+    fontFamily: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif',
+    lineHeight: 1.5,
+    fontWeight: 400,
+
+    fontSynthesis: 'none',
+    textRendering: 'optimizeLegibility',
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Global styles={rootStyle} />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+function rgba(arg0: number, arg1: number, arg2: number, arg3: number) {
+  throw new Error("Function not implemented.");
+}
+
