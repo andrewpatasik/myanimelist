9971259d344014052e7896c05dbdb27c0c1ba5fd
root <root@Andrew-PC>
2023-08-02 14:19:55 +0800

    navbar mobile view added

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,58 @@
 import { css } from "@emotion/react";
 import { colorPalletes, textSizes } from "../style";
+import { Bars2Icon, XMarkIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
 
 const navbarStyle = {
-  self: css({
-    backgroundColor: colorPalletes["green-500"],
-    display: "flex",
+  container: css({
+    position: "fixed",
+    top: "0",
+    width: "100%",
+    color: colorPalletes["white"],
   }),
-  heading: css(textSizes["text-medium"]),
+  self: css({
+    backgroundColor: colorPalletes["green-400"],
+    padding: "1rem",
+    display: "flex",
+    position: "relative",
+  }),
+  icon: css({
+    width: "20px",
+    height: "24px",
+  }),
+  button: css({ all: "unset", ":hover": { cursor: "pointer" } }),
+  heading: css([textSizes["text-small"], { margin: "0 auto 0 auto" }]),
+  menu: css({
+    position: "absolute",
+    display: "flex",
+    width: "100%",
+    height: "112px",
+    backgroundColor: colorPalletes["gray-500"],
+  }),
 };
 
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav css={navbarStyle.self}>
-      <h1 css={navbarStyle.heading}>MYANIMELIST</h1>
-    </nav>
+    <div css={navbarStyle.container}>
+      <nav css={navbarStyle.self}>
+        <button
+          css={navbarStyle.button}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {!isMenuOpen ? (
+            <Bars2Icon css={navbarStyle.icon} />
+          ) : (
+            <XMarkIcon css={navbarStyle.icon} />
+          )}
+        </button>
+        <h1 css={navbarStyle.heading}>MYANIMELIST</h1>
+        <div css={[navbarStyle.icon, { visibility: "hidden" }]}></div>
+      </nav>
+      <div
+        css={[navbarStyle.menu, { display: !isMenuOpen ? "none" : "block" }]}
+      ></div>
+    </div>
   );
 };
